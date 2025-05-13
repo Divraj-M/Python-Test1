b02c2cb74acdf74193873317085234483463c4ed
Divraj-M <2k21cse022@kiot.ac.in>
2025-05-13 21:08:07 +0530

    add the sauce demo for 4 methods

--- saucedemo/read_config.py
+++ saucedemo/read_config.py
@@ -2,6 +2,7 @@ from configparser import ConfigParser
 
 def get_config(category,key):
     config=ConfigParser()
-    config.read("./config.ini")    
+    config.read("C:\\Users\\Lenovo\\Desktop\\Python-Test1\\saucedemo\\config.ini")    
     return config.get(category,key)
+
 
