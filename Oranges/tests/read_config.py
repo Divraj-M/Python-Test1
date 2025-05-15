f4e9a8b29ea231fb74965e519c442746e5ce1726
Divraj-M <2k21cse022@kiot.ac.in>
2025-05-15 08:08:33 +0530

    created the orange class

---
+++ Oranges/tests/read_config.py
@@ -0,0 +1,8 @@
+from configparser import ConfigParser
+
+def get_config(category,key):
+    config=ConfigParser()
+    config.read("C:\\Users\\Lenovo\\Desktop\\Orange-Python\\Oranges\\tests\\config.ini")    
+    return config.get(category,key)
+
+
